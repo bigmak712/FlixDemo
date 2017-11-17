ec31ee1decbeb158cfb7c13e150a5c534f6055f0
bigmak712 <bigmak712@gmail.com>
2017-11-17 11:36:35 -0800

    setup collection view

---
+++ FlixDemo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  FlixDemo
+//
+//  Created by Timothy Mak on 11/17/17.
+//  Copyright © 2017 Timothy Mak. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
